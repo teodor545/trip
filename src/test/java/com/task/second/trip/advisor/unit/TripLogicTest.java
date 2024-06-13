34dcb16ca4ec77702cace971da04138ee0c9cb1f
teodor545 <teodorvlv@gmail.com>
2024-06-13 21:31:13 +0300

    Improve test

--- src/test/java/com/task/second/trip/advisor/unit/TripLogicTest.java
+++ src/test/java/com/task/second/trip/advisor/unit/TripLogicTest.java
@@ -5,21 +5,33 @@ import com.task.second.trip.advisor.dto.TripRequestDto;
 import com.task.second.trip.advisor.service.TripServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class TripLogicTest {
 
     private final CountryData countryData = new CountryData();
     private final TripServiceImpl tripService = new TripServiceImpl(null, null, null);
 
-    @Test
-    public void testTripLogic_basic_BG() throws Exception {
-        TripRequestDto tripRequestDto = new TripRequestDto("BG", 100d, 1200d, "BGN");
+    private static Stream<Arguments> testParamsBasic() {
+        return Stream.of(
+                Arguments.of(new TripRequestDto("BG", 100d, 1200d, "BGN"), 2 ,200),
+                Arguments.of(new TripRequestDto("GR", 100d, 500d, "EUR"), 1 ,100),
+                Arguments.of(new TripRequestDto("MC", 1d, 20d, "EUR"), 20 ,0));
+    }
 
-        Map<String, String> borders = countryData.findBorderCountries("BG").getLandBorders();
+    @ParameterizedTest
+    @MethodSource("testParamsBasic")
+    public void testTripLogic_basic(TripRequestDto tripRequestDto, double expectedTripCount, double expectedLeftOverAmount) throws Exception {
+        Map<String, String> borders = countryData.findBorderCountries(tripRequestDto.getStartingCountryCode()).getLandBorders();
 
         Method calculateTrip = tripService.getClass().getDeclaredMethod("calculateTrip", TripRequestDto.class, Map.class);
         calculateTrip.setAccessible(true);
@@ -35,7 +47,7 @@ public class TripLogicTest {
 
         double leftOverAmount = (double) leftOverAmountField.get(result);
 
-       Assertions.assertEquals(2, tripCount);
-       Assertions.assertEquals(200, leftOverAmount);
+        Assertions.assertEquals(expectedTripCount, tripCount);
+        Assertions.assertEquals(expectedLeftOverAmount, leftOverAmount);
     }
 }
